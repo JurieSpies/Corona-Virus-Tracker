2ddf97c7c571e6af19a07ad7cfd526734840401c
Wilco Breedt <wilco.breedt@openvantage.co.za>
2020-02-20 18:12:37 +0200

    Change Icon & Splash Sreen

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 //Packages
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 //pages
 import Navigator from './src/Router/Router';
 
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return <Navigator />;
   }
